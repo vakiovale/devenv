3bd1aa4a5530f4e1b58f6ed3540fde010aab50bb
Valtteri Mäkikoski <valtteri.makikoski@gmail.com>
2020-10-23 16:26:01 +0300

    Add basic playbook and script for installing some packages

---
+++ install_docker_deps.sh
@@ -0,0 +1,8 @@
+## https://docs.docker.com/install/linux/docker-ce/ubuntu/ 
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
